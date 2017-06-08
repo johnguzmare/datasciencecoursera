4839e9fefeece3d39579b39053800fbc097fb849
johnguzmare <john.guzmare@gmail.com>
2017-06-08 10:03:48 -0500

    Create SwirlPracticalR.d

---
+++ R-Programming/Week2/SwirlPracticalR.d
@@ -0,0 +1,44 @@
+Practical R excecises in swirl
+=================================
+
+Isntall Swirl
+----------------
+Since swirl is an R package, you can easily install it by entering a single command from the R console:
+
+```r
+install.packages("swirl")
+```
+If you've installed swirl in the past make sure you have version 2.2.21 or later. You can check this with:
+
+
+```r
+packageVersion("swirl")
+```
+
+Load swirl
+----------------
+
+Every time you want to use swirl, you need to first load the package. From the R console:
+
+```r
+library(swirl)
+```
+
+Install the R Progroamming course
+----------------
+
+swirl offers a variety of interactive courses, but for our purposes, you want the one called R Programming. Type the following from the R prompt to install this course:
+
+```r
+install_from_swirl("R Programming")
+```
+
+Start swirl and complete the lessons
+------------------
+Type the following from the R console to start swirl:
+
+```r
+swirl()
+```
+
+
